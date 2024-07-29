fa37537a64c75d1d28033ee85971ebbd5e1cca12
Adnan Siddiqui <sid.adnan9569@gmail.com>
2024-07-29 20:41:16 +0530

    Add files via upload

---
+++ AA1.java
@@ -0,0 +1,11 @@
+// Since every class is the child class of this Object class 
+//so this method can be overriden by this class
+public class AA1 
+{
+	public static void main(String[] args) 
+	{
+		AA obj=new AA();
+		System.out.println(obj);
+	}
+
+}
