9e1aae4828eee78421e8fdd6b6c818c2e74902c9
Adnan Siddiqui <sid.adnan9569@gmail.com>
2024-07-29 20:40:52 +0530

    Update AA.java

--- AA.java
+++ AA.java
@@ -1,2 +1,12 @@
+//toString() is the method of Object class .So every class has this method 
+//It returns the combination of the following three values
+// 1. Class Name
+// 2. @
+// 3. Hexadecimal value of object hashcode
 public class AA 
-{}
+{
+	public String toString()
+	{
+		return "Im Adnan Siddiuqi";
+	}
+}
